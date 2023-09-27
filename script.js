aeba26cf224eb53bef272ec2c244441037fb1081
RavitejaMulaparthi <137281804+RavitejaMulaparthi@users.noreply.github.com>
2023-09-27 16:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-//Add your code here
-	const totalPrice = item.reduce((sum, item) => sum + item.price, 0);
+/* Your CSS Code here. */
+        function calculateTotalPrice() {
+            const priceElements = document.querySelectorAll('.prices');
+            let totalPrice = 0;
 
-  // Display the total price
-  alert(`Total Price: $${totalPrice}`);
-  
-};
+            // Iterate through price elements and calculate the sum
+            priceElements.forEach((priceElement) => {
+                const price = parseFloat(priceElement.innerText);
+                if (!isNaN(price)) {
+                    totalPrice += price;
+                }
+            });
 
-getSumBtn.addEventListener("click", getSum);
+            // Create a new row to display the total price
+            const table = document.querySelector('table');
+            const newRow = document.createElement('tr');
+            const newCell = document.createElement('td');
+            newCell.innerText = `Total Price: ${totalPrice.toFixed(2)}`;
+            newRow.appendChild(newCell);
+            table.appendChild(newRow);
+        }
 
+        // Calculate the total price when the page loads
+getSumBtn.addEventListener("click", calculateTotalPrice());
